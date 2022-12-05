959ca04eaa75a9f994c481a1fdeca693c430b0d4
denis <eranin.d@yandex.ru>
2022-12-05 16:48:52 +0300

    cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String animal;
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setMessage(String animal) {
+        this.animal = animal;
+    }
+}
